dd035e7e23e101b044633d8883ced75d13450a47
Jackson Kasi <jacksonkasi@outlook.com>
2025-02-09 16:30:19 +0000

    undo

--- examples/web/postcss.config.mjs
+++ examples/web/postcss.config.mjs
@@ -1,10 +1,7 @@
+// postcss.config.mjs
 export default {
   plugins: [
     'tailwindcss',
     'autoprefixer',
-    [
-      (await import('./postcss-extract-tailwind.js')).default,
-      { output: 'public/extracted-tailwind.css' }
-    ]
   ]
-}
+};
